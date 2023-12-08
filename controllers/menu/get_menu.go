7d25d56aa3a45f9ac9cccbba1343b32b4098ff0d
Manhnedungso <myytb20095511@gmail.com>
2023-12-08 20:46:39 +0700

    [Controllers] Complete menu feature

---
+++ controllers/menu/get_menu.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	collection "restaurant_management/controllers/collections"
+	"restaurant_management/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		menuId := c.Param("menu_id")
+		var menu models.Menu
+
+		err := collection.MenuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the menu"})
+		}
+		c.JSON(http.StatusOK, menu)
+	}
+}
